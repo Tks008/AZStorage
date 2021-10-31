e420ef538df184f4cb40d482f73a5f57b94fbd9f
Tushar <86506163+Tks008@users.noreply.github.com>
2021-10-31 08:35:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Queues;
+using System;
+
+namespace AZStorage
+{
+    class Program
+    {
+        static string connectionstring = "DefaultEndpointsProtocol=https;AccountName=tksstorageaccount;AccountKey=yM4b9KOYPcEPbgayFfEm68KBST0NUA4gtTapET1c/Ix1XxTe6dKIMDjnxuD0NElTyloVkYLxQCfnGKwyVTpBmg==;EndpointSuffix=core.windows.net";
+        static string queuname = "tksqueue";
+        static void Main(string[] args)
+        {
+            QueueClient queueclient = new QueueClient(connectionstring, queuname);
+
+            string _mesg = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                queueclient.SendMessage($"Message {i + 1}");
+            }
+
+            Console.WriteLine("Message sent.");
+            Console.ReadKey();
+        }
+    }
+}
